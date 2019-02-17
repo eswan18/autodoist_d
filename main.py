97ef88f175708280c2f057cd9f6fce31465507e6
Ethan Swan <eswan18@rocketmail.com>
2019-02-17 18:26:32 -0500

    Begin transition to schedule module.
    Update dependencies; started moving functionality into a schedule-based
    loop. Have yet to figure out how to let users specify a schedule in a
    way that can be translated into this schedule module's language.

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
+# Builtins
 import os
+import time
+# Installed
 import yaml
 import todoist
+import schedule
+# Project libraries
 import utils
 
 api_token = os.environ['TODOIST_API_TOKEN']
@@ -19,25 +24,34 @@ labels = api.labels.all()
 projects = api.projects.all()
 items = api.items.all()
 
-####################################################################
-# Auto-label items in projects
-####################################################################
-for pl_map in conf['project-label']:
-    # Find the matching project and labels and get their IDs.
-    project = utils.get_project_by_name(pl_map['project'], projects)
-    label = utils.get_label_by_name(pl_map['label'], labels)
-    project_items = utils.get_items_by_project_id(project['id'], items)
-    # Make sure every item has the required label. If not, add it.
-    for item in project_items:
-        if label['id'] not in item['labels']:
-            item.update(labels=item['labels'] + [label['id']])
-api.commit()
+
+
+def update():
+    ####################################################################
+    # Auto-label items in projects
+    ####################################################################
+    for pl_map in conf['project-label']:
+        # Find the matching project and labels and get their IDs.
+        project = utils.get_project_by_name(pl_map['project'], projects)
+        label = utils.get_label_by_name(pl_map['label'], labels)
+        project_items = utils.get_items_by_project_id(project['id'], items)
+        # Make sure every item has the required label. If not, add it.
+        for item in project_items:
+            if label['id'] not in item['labels']:
+                item.update(labels=item['labels'] + [label['id']])
+    api.commit()
 
 ####################################################################
 # Instantiate templates
 ####################################################################
-for template in conf['template-instantiations']:
-    project = utils.get_project_by_name(template['existing-project-name'],
-                                        projects)
-    template_filename = CONFIG_DIR + '/' + template['template-file']
-    api.templates.import_into_project(project['id'], template_filename)
+#for template in conf['template-instantiations']:
+#    project = utils.get_project_by_name(template['existing-project-name'],
+#                                        projects)
+#    template_filename = CONFIG_DIR + '/' + template['template-file']
+#    api.templates.import_into_project(project['id'], template_filename)
+
+# Use the schedule module to handle the looping.
+schedule.every(10).minutes.do(update)
+while True:
+    schedule.run_pending()
+    time.sleep(60)
--- utils.py
+++ utils.py
@@ -4,11 +4,13 @@ def get_project_by_name(p_name, projects):
                if project['name'] == p_name][0]
     return project
 
+
 def get_label_by_name(l_name, labels):
     '''Find and return a label with name `l_name` from within `labels`.'''
     label = [label for label in labels
-               if label['name'] == l_name][0]
+             if label['name'] == l_name][0]
     return label
+
 
 def get_items_by_project_id(p_id, items):
     return [item for item in items
