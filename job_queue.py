6a2302ea88c8e6521ec10c2a1f241720bce5d36e
Ethan Swan <eswan18@rocketmail.com>
2019-02-20 20:16:32 -0500

    Add docstrings to job_queue.py

--- job_queue.py
+++ job_queue.py
@@ -3,6 +3,9 @@ from croniter import croniter
 import pytz
 
 class JobQueue():
+    '''
+    A queue of jobs to be run at regular intervals.
+    '''
 
     def __init__(self, jobdict=None):
         if jobdict is None:
@@ -10,11 +13,18 @@ class JobQueue():
         self.jobdict = jobdict
 
     def add_job(self, job_name, job_func, job_cron):
+        '''
+        Add a new job to the queue.
+
+        :param job_name: a name for the job
+        :param job_func: a callable to be executed when the job is run
+        :param job_cron: a string in cron-format indicating when to run the job
+        '''
         if job_name in self.jobdict:
             raise ValueError(f'Job name {job_name} already exists.')
         # Convert the current time to UTC so the math works.
         current_time = datetime.now().astimezone(pytz.utc)
-        # Create a croniter object for the job
+        # Create a croniter object for the job.
         job_iter = croniter(job_cron, current_time)
         # Build a dictionary of one key, which we can use to update the master
         # jobdict.
@@ -27,6 +37,9 @@ class JobQueue():
         self.jobdict.update(job)
 
     def run_pending(self):
+        '''
+        Run all jobs whose next run is due to be kicked off.
+        '''
         for name, job in self.jobdict.items():
             # Execute any jobs that are due.
             if job['next'] < datetime.now().astimezone(pytz.utc):
