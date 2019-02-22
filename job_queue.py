e20791edb49d17596ca832faafadeffc4ada17f6
Ethan Swan <eswan18@rocketmail.com>
2019-02-22 08:28:53 -0500

    Add optional day_or flag to JobQueue init.
    For better compliance with the croniter package standard, added optional
    day_or flag to determine whether cron should AND day-of-week and date or
    OR them.

--- job_queue.py
+++ job_queue.py
@@ -13,7 +13,7 @@ class JobQueue():
             jobdict = {}
         self.jobdict = jobdict
 
-    def add_job(self, job_name, job_func, job_cron):
+    def add_job(self, job_name, job_func, job_cron, day_or=True):
         '''
         Add a new job to the queue.
 
@@ -26,12 +26,13 @@ class JobQueue():
         # Convert the current time to UTC so the math works.
         current_time = datetime.now().astimezone(pytz.utc)
         # Create a croniter object for the job.
-        job_iter = croniter(job_cron, current_time)
+        job_iter = croniter(job_cron, current_time, day_or=day_or)
         # Build a dictionary of one key, which we can use to update the master
         # jobdict.
         job = {job_name: {'func': job_func,
                           'cron': job_cron,
                           'iter': job_iter,
+                          'day_or': day_or,
                           'next': job_iter.get_next(datetime)
                           }
               }
@@ -44,7 +45,7 @@ class JobQueue():
         for name, job in self.jobdict.items():
             # Execute any jobs that are due.
             if job['next'] < datetime.now().astimezone(pytz.utc):
-                # First, update the next value.
+                # Update the next time.
                 job['next'] = job['iter'].get_next(datetime)
                 # Then run the function.
                 job['func']()
