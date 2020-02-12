f1ccc41dadad897c0083a93ff5710adffe33e814
Ethan Swan <eswan18@rocketmail.com>
2020-02-12 07:35:09 -0500

    Get alert job working with terminal printing.
    Still need to add the email bit.

--- src/jobs.py
+++ src/jobs.py
@@ -1,5 +1,13 @@
 from typing import Callable
 from dataclasses import dataclass
+import logging
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+# Console Handler
+ch = logging.StreamHandler()
+formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+ch.setFormatter(formatter)
 
 def update_templates(user, conf):
     '''Automatically label tasks in projects.
@@ -12,7 +20,7 @@ def update_templates(user, conf):
         An application configuration. Specifically needs to have a
         project-label key.
     '''
-    #logger.info('Started update job.')
+    logger.info('Started update job.')
     for record in conf['project-label']:
         project_name, label_name = record['project'], record['label']
         # Tasks in the project in question.
@@ -30,12 +38,37 @@ def update_templates(user, conf):
                 log_str = log_str.format(task.content,
                                          project_name,
                                          label_name)
-                #logger.debug(log_str)
-    #logger.info('Completed update job.')
+                logger.debug(log_str)
+    logger.info('Completed update job.')
+
+def missing_due_date_alert(user, conf):
+    print('Starting missing due date alert')
+    # We need to keep track of the tasks we've already seen -- can be done by
+    # setting an attribute of the function.
+    try:
+        seen_ids = missing_due_date_alert.seen_ids
+    # The first time the function runs, this attribute won't exist yet and
+    # needs to be initialized.
+    except AttributeError:
+        seen_ids = []
+    # Get uncompleted tasks without a due date.
+    undue_tasks = [task for task in user.get_tasks()
+                   if not task.checked and task.due is None]
+    # Determine which haven't been seen before, and record them.
+    unseen_tasks = [task for task in undue_tasks
+                    if task.id not in seen_ids]
+    seen_ids.extend([task.id for task in unseen_tasks])
+    missing_due_date_alert.seen_ids = seen_ids
+    #missing_due_date_alert.seen_ids = seen_ids
+    for task in unseen_tasks:
+        s = (f'You added a task "{task.content}" to project '
+             f'{task.project.name} without a due date. Was this intentional?')
+        print(s)
+
 
 # Create a record class.
-# I would have liked to use a named tuple here but the immutability of its
-# attributes turned out to be annoying.
+# I would have liked to use a named tuple here but the immutability turned out
+# to be annoying.
 @dataclass
 class FuncJob:
     '''Class for tracking job definitions.'''
@@ -44,4 +77,8 @@ class FuncJob:
     cron: str
 
 # Add all jobs into a single list.
-jobs = [FuncJob('Update Templates', update_templates, '*/5 * * * *')]
+jobs = [FuncJob('Update Templates', update_templates, '*/5 * * * *'),
+        FuncJob('Missing Due Date Alert', missing_due_date_alert, '* * * * *')]
+# Note that this could be more elegantly implemented as a decorator for each
+# task function.
+
