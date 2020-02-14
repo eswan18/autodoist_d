da57f9fbb076bdb09041aa5de6cbd4a227ecf654
Ethan Swan <eswan18@rocketmail.com>
2020-02-14 12:26:53 +0000

    Add scripts for docker build & run.

---
+++ run.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+docker run \
+    -e EMAIL_ADDR -e EMAIL_PW -e TODOIST_API_TOKEN \
+    -d \
+    --restart always \
+    --mount type=bind,source=$PWD/config,target=/autodoist/config \
+    autodoist
